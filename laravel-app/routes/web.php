34197d4a18481ce86874ce8dd27e0e83d9a656db
moeurn chheng <chheng6686@gmail.com>
2026-07-22 13:41:37 +0700

    db to migrate & chat be

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user/{userId}/chats', function ($userId) {
+    $user = User::find($userId);
+    return response()->json([
+        'user' => $user,
+        'chats' => $user->chats()->get(),
+        'created_chats' => $user->createdChats()->get(),
+    ]);
 });
